261e725b6b5178bc5669ab40145adff58b5d7663
grvela <rodrigoandre777@gmail.com>
2024-04-16 23:17:47 -0400

    feat(main): updanting main files and maven dependencies

--- src/main/java/com/mazzatech/admazsshipping/AdmAzsShippingApplication.java
+++ src/main/java/com/mazzatech/admazsshipping/AdmAzsShippingApplication.java
@@ -1,9 +1,12 @@
-package com.mazzatech.admazsshipping.application.config;
+package com.mazzatech.admazsshipping;
 
+import com.mazzatech.admazsshipping.persistence.interfaces.ShippingMongoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = ShippingMongoRepository.class)
 public class AdmAzsShippingApplication {
 
 	public static void main(String[] args) {
